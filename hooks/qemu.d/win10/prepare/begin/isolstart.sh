f2bed86bb04a125076fad92ed03f11c32ea12ff3
Sajay <78424701+sajayprakash@users.noreply.github.com>
2022-01-01 08:38:53 +0530

    Delete isolstart.sh

--- hooks/qemu.d/win10/prepare/begin/isolstart.sh
+++
@@ -1,3 +0,0 @@
-systemctl set-property --runtime -- user.slice AllowedCPUs=0,5
-systemctl set-property --runtime -- system.slice AllowedCPUs=0,5
-systemctl set-property --runtime -- init.scope AllowedCPUs=0,5
